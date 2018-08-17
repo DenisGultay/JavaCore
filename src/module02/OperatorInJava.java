63369a59ca2c6823d5f70d7786bc643ca686806d
DenisGultay <denisgultay@gmail.com>
2018-08-17 13:51:59 +0300

    module02 first commit

---
+++ src/module02/OperatorInJava.java
@@ -0,0 +1,19 @@
+package module02;
+
+/**
+ * Created by denis on 17.08.18.
+ */
+public class OperatorInJava {
+    public static void main(String[] args) {
+
+        int integer1 = 50;
+        int integer2 = 10;
+
+        int integer3 = integer1 + integer2;
+
+        System.out.println(integer3);
+
+        integer3 = integer1 / integer2;
+    }
+
+}
---
+++ src/module02/TypesInJava.java
@@ -0,0 +1,27 @@
+package module02;
+
+/**
+ * Created by denis on 16.08.18.
+ */
+public class TypesInJava {
+
+    public static void main(String[] args) {
+
+        byte byteType = 0; // 0000 0000
+
+        int intType;
+
+        intType = 598;
+
+        char charType = '%';
+
+        intType = charType;
+
+        char charType2 = 47568;
+
+        System.out.println(charType);
+        System.out.println(intType);
+        System.out.println(charType2);
+
+    }
+}
