a48f73d31c3aa31b5a4bf7fd3db594704252a97e
DenisGultay <denisgultay@gmail.com>
2018-08-17 18:43:59 +0300

    module02 second commit

--- src/module02/OperatorInJava.java
+++ src/module02/OperatorInJava.java
@@ -14,6 +14,25 @@ public class OperatorInJava {
         System.out.println(integer3);
 
         integer3 = integer1 / integer2;
+
+        System.out.println(integer3);
+
+        double doubleValue = (double)integer2 / integer1;
+
+        System.out.println(doubleValue);
+
+        int integer4 = 100;
+
+        integer4 += 20;
+
+        System.out.println(integer4);
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        boolean bool3 = bool1 || bool2;
+
+        System.out.println(bool3);
     }
 
 }
